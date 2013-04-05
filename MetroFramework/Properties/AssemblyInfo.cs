15809993e83a1d2d85e18f4243b68001829475ae
Jens <thielj@gmail.com>
2013-04-05 23:04:20 +0100

    Updated README and licenses
    Re-licenses under same license (MIT new).
    Attribution as required by original license.

--- MetroFramework/Properties/AssemblyInfo.cs
+++ MetroFramework/Properties/AssemblyInfo.cs
@@ -1,4 +1,30 @@
-﻿using System;
+﻿/*
+ 
+MetroFramework - Modern UI for WinForms
+
+Copyright (c) 2013 Jens Thiel, http://github.com/thielj/winforms-modernui
+Portions of this software are Copyright (c) 2011 Sven Walter, http://github.com/viperneo
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of 
+this software and associated documentation files (the "Software"), to deal in the 
+Software without restriction, including without limitation the rights to use, copy, 
+modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, 
+and to permit persons to whom the Software is furnished to do so, subject to the 
+following conditions:
+
+The above copyright notice and this permission notice shall be included in 
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, 
+INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A 
+PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT 
+HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF 
+CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE 
+OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ 
+ */
+
+using System;
 using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
@@ -49,8 +75,8 @@ internal static class MetroFrameworkAssembly
     internal const string Title = "MetroFramework.dll";
     internal const string Version = "1.2.0.0";
     internal const string Description = "Metro UI Framework for .NET WinForms";
-    internal const string Copyright = "Copyright \x00a9 2011 Sven Walter.  All rights reserved.";
-    internal const string Company = "Sven Walter";
+    internal const string Copyright = "Copyright \x00a9 2013 Jens Thiel. Portions of this software are Copyright \x00a9 2011 Sven Walter.  All rights reserved.";
+    internal const string Company = "Jens Thiel";
     internal const string Product = "MetroFramework";
 }
 
